b476ac390b545ec7cf8220e151042413bc23751f
Susztek Norbert <susztek.norbert@gmail.com>
2023-03-28 00:56:55 +0200

    Revert "init"
    This reverts commit fa4012f34d28c7cbcebacd80102846e3b04e3f47.

--- src/main/java/utilityClasses/testSetup/deviceSetup/BrowserTypeBase.java
+++
@@ -1,7 +0,0 @@
-package utilityClasses.testSetup.deviceSetup;
-
-import org.openqa.selenium.WebDriver;
-
-public interface  BrowserTypeBase {
-    public WebDriver browserSelection(String browser)throws Exception;
-}
