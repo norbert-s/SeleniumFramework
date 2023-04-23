acc1492d8c87cc322ea5a0f9f0b442c10d9ddfb8
Susztek Norbert <susztek.norbert@gmail.com>
2023-04-23 16:43:25 +0200

    extent report

--- src/main/java/testSetup/deviceSetup/base/DriverBaseClass.java
+++ src/main/java/testSetup/deviceSetup/base/DriverBaseClass.java
@@ -1,12 +1,16 @@
 package testSetup.deviceSetup.base;
 
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
+import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 import org.testng.annotations.*;
@@ -17,7 +21,12 @@ import testSetup.setters.WrapperToCallSetupMethods;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
+
 import org.testng.asserts.SoftAssert;
+
+import static testSetup.deviceSetup.base.ExtentTestManager.getTest;
+
 @Slf4j
 public  class DriverBaseClass extends DriverBaseClassAbstract{
 
@@ -43,7 +52,76 @@ public  class DriverBaseClass extends DriverBaseClassAbstract{
         getDriverManager().quitDriver();
         softAssert.remove(); // Clean up SoftAssert instance
     }
-    public static class TestListener extends TestListenerAdapter {
+
+    public static class TestListener  implements ITestListener {
+        private synchronized static String getTestMethodName(ITestResult iTestResult) {
+            return iTestResult.getMethod().getConstructorOrMethod().getName();
+        }
+        @Override
+        public synchronized void onStart(ITestContext iTestContext) {
+            log.info("I am in onStart method " + iTestContext.getName());
+        }
+
+        @Override
+        public synchronized void onTestStart(ITestResult iTestResult) {
+            log.info(getTestMethodName(iTestResult) + " test is starting.");
+            // Set the driver attribute for the test instance
+            Object testInstance = iTestResult.getInstance();
+            if (testInstance instanceof DriverBaseClass) {
+                DriverBaseClass driverBaseClass = (DriverBaseClass) testInstance;
+                iTestResult.getTestContext().setAttribute("WebDriver", driverBaseClass.getDriver());
+            }
+            // Start a new test in ExtentTestManager
+            ExtentTestManager.startTest(iTestResult.getMethod().getMethodName(), "");
+        }
+
+        // ...
+
+        @Override
+        public synchronized void onTestFailure(ITestResult iTestResult) {
+            log.info(getTestMethodName(iTestResult) + " test is failed.");
+            // Get driver from the test instance
+            Object testInstance = iTestResult.getInstance();
+            if (testInstance instanceof DriverBaseClass) {
+                DriverBaseClass driverBaseClass = (DriverBaseClass) testInstance;
+                WebDriver driver = driverBaseClass.getDriver();
+                // Take base64Screenshot screenshot for extent reports
+                String base64Screenshot =
+                        "data:image/png;base64," + ((TakesScreenshot) Objects.requireNonNull(driver)).getScreenshotAs(OutputType.BASE64);
+                // ExtentReports log and screenshot operations for failed tests.
+                getTest().log(Status.FAIL, "Test Failed",
+                        getTest().addScreenCaptureFromBase64String(base64Screenshot).getModel().getMedia().get(0));
+            }
+        }
+        @Override
+        public synchronized void onFinish(ITestContext iTestContext) {
+            log.info("I am in onFinish method " + iTestContext.getName());
+            //Do tier down operations for ExtentReports reporting!
+            ExtentManager.extentReports.flush();
+        }
+
+        @Override
+        public synchronized void onTestSuccess(ITestResult iTestResult) {
+            log.info(getTestMethodName(iTestResult) + " test is succeed.");
+            //ExtentReports log operation for passed tests.
+            getTest().log(Status.PASS, "Test passed");
+        }
+
+        @Override
+        public synchronized void onTestSkipped(ITestResult iTestResult) {
+            log.info(getTestMethodName(iTestResult) + " test is skipped.");
+            //ExtentReports log operation for skipped tests.
+            getTest().log(Status.SKIP, "Test Skipped");
+        }
+        @Override
+        public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+            log.info("Test failed but it is in defined success ratio " + getTestMethodName(iTestResult));
+        }
+    }
+
+
+
+    public static class TestListener2 extends TestListenerAdapter {
         public synchronized void takeScreenshot(WebDriver driver, String fileName) {
             File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             try {
